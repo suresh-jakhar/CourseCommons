fd377a668b21af49d710eaf6aab66a5283fb5985
Suresh Jakhar <gettingsuresh@gmail.com>
2026-03-24 19:48:28 +0530

    feat: add / route handler that responds with 'App is running'

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ const app = express();
 app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
 
+
+app.get('/', (req, res) => {
+    res.send('App is running');
+});
+
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/course", courseRoutes);
 app.use("/api/v1/instructor", instructorRoutes);
